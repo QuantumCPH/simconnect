74c56b321824e82d6b946c24b293a4b18c00af7f
khanmuhammad <khan.muhammad@zerocall.com>
2013-03-20 17:53:11 +0500

    sdjs

--- apps/backend/modules/company/templates/_edit_form.php
+++ apps/backend/modules/company/templates/_edit_form.php
@@ -82,7 +82,8 @@
 
   <?php $value = object_input_tag($company, 'getPostCode', array (
   'size' => 80,
-
+   'minlength'=>3,
+   'maxlength'=>7,
   'control_name' => 'company[post_code]',
 )); echo $value ? $value : '&nbsp;' ?><br><label id="companyPost"></label>
     </div>
